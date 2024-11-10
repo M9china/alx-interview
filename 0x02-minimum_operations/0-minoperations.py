375025be870e8b894b318daf2672bcb629d5dcfc
M9china <nqobilencaza2@gmail.com>
2024-11-10 19:15:52 +0200

    remove tralling white spaces

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,18 +3,19 @@
 Defines minimum operations function
 """
 
+
 def minOperations(n):
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         # While the divisor divides n, reduce n and count the operations
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-        
+ 
     return operations
