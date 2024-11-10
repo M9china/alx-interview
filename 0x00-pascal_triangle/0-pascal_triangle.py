88353d1ea77474158375807675e766abb7714eee
M9china <nqobilencaza2@gmail.com>
2024-11-10 11:27:23 +0200

    addd funtion to create pascal triamgle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A python function for pascals triangle"""
+
+
+def pascal_triangle(n):
+    """This function creates Pascals Triangle
+    Parameters:
+        x: represents a row
+    Returns:
+        pascals_triangle (list): which is an empty list"""
+
+    # Empty list to hold the triangle
+    pascals_triangle = []
+    if n <= 0:
+        return pascals_triangle
+
+    for x in range(n):
+        # Start each row with a list
+        current_row = []
+        # First number in every row is always 1
+        current_row.append(1)
+        # Fill in the middle numbers
+        if x > 0:
+            # Calculate middle numbers if x > 0
+            if x > 1:
+                # Only calculate middle numbers if it's not the first two rows
+                last_row = pascals_triangle[x - 1]
+                for j in range(1, x):
+                    # Each number is the sum of the two numbers above it
+                    current_row.append(last_row[j - 1] + last_row[j])
+    # Last number in every row is always 1 (except the first row)
+        if x > 0:
+            current_row.append(1)
+    # Add the current row to the triangle
+        pascals_triangle.append(current_row)
+
+    return pascals_triangle
